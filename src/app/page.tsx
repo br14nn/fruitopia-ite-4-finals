fd0e3c2c764143d524125745ef80108f22ee9f9f
br14nn <vituallabrian727@gmail.com>
2024-05-06 19:04:00 +0800

    product card in development

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import { BsBagPlusFill } from "react-icons/bs";
+import { IconContext } from "react-icons";
 
 import CustomSection from "@/components/CustomSection";
+import { ProductCard } from "@/components/ProductCard";
 
 export default async function Home() {
   return (
-    <main className="mx-auto flex w-full max-w-[1920px] flex-col items-center justify-center">
+    <main className="mx-auto flex w-full max-w-[1920px] flex-col items-center justify-center bg-white">
       <CustomSection className="flex h-[100svh] flex-col items-start justify-center gap-4 bg-[url('../assets/images/hero-bg.jpg')] bg-cover bg-no-repeat px-4 leading-none md:items-center">
         <h1 className="text-3xl font-bold tracking-wider text-white lg:text-5xl">
           Welcome to Fruitopia
@@ -18,7 +21,9 @@ export default async function Home() {
           Where Every Bite Ignites a Symphony of Flavor, Health, and Joy!
         </h2>
       </CustomSection>
-      <CustomSection className="px-4 py-8"></CustomSection>
+      <CustomSection className="mx-auto grid max-w-[1536px] grid-cols-2 flex-col gap-4 p-4">
+        <ProductCard />
+      </CustomSection>
     </main>
   );
 }
